46ee71cf5d7a671c589602f6c0c09b46f4f6eeae
Mark Jaquith <mark.jaquith@gusto.com>
2025-06-05 13:37:13 -0400

    Skip mise if SKIP_MISE is set

--- .zsh/060-mise.zsh
+++ .zsh/060-mise.zsh
@@ -1,4 +1,8 @@
 # Configure HK to use Mise
 # See: https://hk.jdx.dev/mise_integration.html
 export HK_MISE=1
-eval "$(mise activate zsh)"
+
+# Load Mise unless SKIP_MISE is set.
+if [[ -z "$SKIP_MISE" ]]; then
+	eval "$(mise activate zsh)"
+fi
