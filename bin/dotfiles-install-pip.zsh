cc7f98eadf55882742c70afe36658266161429d2
mark jaquith <mark.jaquith@gusto.com>
2025-10-31 11:19:48 -0400

    Update jrnl installation

--- bin/dotfiles-install-pip.zsh
+++ bin/dotfiles-install-pip.zsh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh
-# Bulk pip and pipx install
+# Bulk pip and uv tool install
 
 pip install --quiet \
     faker 2>&1 | grep -v "DEPRECATION"
 
-# pipx installs
-pipx install --force jrnl > /dev/null 2>&1
+# uv tool installs
+uv tool install jrnl > /dev/null 2>&1
