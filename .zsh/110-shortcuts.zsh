4ba0c7857e675385464ad759c158f7a19e474785
Mark Jaquith <mark@jaquith.me>
2026-01-01 01:34:22 -0500

    feat(zsh): add edit-command-line keybinding

--- .zsh/110-shortcuts.zsh
+++ .zsh/110-shortcuts.zsh
@@ -1,3 +1,8 @@
+# Open buffer line in editor
+autoload -Uz edit-command-line
+zle -N edit-command-line
+bindkey '^xe' edit-command-line
+
 bindkey "[D" backward-word
 bindkey "[C" forward-word
 bindkey "^[a" beginning-of-line
