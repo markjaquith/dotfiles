929995265bcd2f5fd56b822e01df501022d3382c
Mark Jaquith <mark@jaquith.me>
2026-01-01 02:05:08 -0500

    chore(zsh): switch to emacs keybindings and simplify prompt

--- .p10k.zsh
+++ .p10k.zsh
@@ -97,7 +97,7 @@
     midnight_commander      # midnight commander shell (https://midnight-commander.org/)
     nix_shell               # nix shell (https://nixos.org/nixos/nix-pills/developing-with-nix-shell.html)
     chezmoi_shell           # chezmoi shell (https://www.chezmoi.io/)
-    vi_mode                 # vi mode (you don't need this if you've enabled prompt_char)
+    # vi_mode               # vi mode (disabled - using emacs mode instead)
     # vpn_ip                # virtual private network indicator
     # load                  # CPU load
     # disk_usage            # disk usage
@@ -203,23 +203,13 @@
   ################################[ prompt_char: prompt symbol ]################################
   # Transparent background.
   typeset -g POWERLEVEL9K_PROMPT_CHAR_BACKGROUND=
-  # Green prompt symbol for insert vi mode.
-  typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIINS_FOREGROUND='#a6da95'
-	# Blue prompt symbol for normal vi mode.
-  typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VICMD_FOREGROUND='#8aadf4'
-	# Pink prompt symbol for visual vi mode.
-  typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIVIS_FOREGROUND='#c6a0f6'
-	# Orange prompt symbol for overwrite vi mode.
-  typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIOWR_FOREGROUND='#ee99a0'
+  # Green prompt symbol when last command succeeded.
+  typeset -g POWERLEVEL9K_PROMPT_CHAR_OK_VIINS_FOREGROUND='#a6da95'
+  # Red prompt symbol when last command failed.
+  typeset -g POWERLEVEL9K_PROMPT_CHAR_ERROR_VIINS_FOREGROUND='#ed8796'
   # Default prompt symbol.
-  typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIINS_CONTENT_EXPANSION=' ❯'
-  # Prompt symbol in command vi mode.
-  typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VICMD_CONTENT_EXPANSION='󰬕❯'
-  # Prompt symbol in visual vi mode.
-  typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIVIS_CONTENT_EXPANSION='❯'
-  # Prompt symbol in overwrite vi mode.
-  typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIOWR_CONTENT_EXPANSION='󰬖❯'
-  typeset -g POWERLEVEL9K_PROMPT_CHAR_OVERWRITE_STATE=true
+  typeset -g POWERLEVEL9K_PROMPT_CHAR_OK_VIINS_CONTENT_EXPANSION='❯'
+  typeset -g POWERLEVEL9K_PROMPT_CHAR_ERROR_VIINS_CONTENT_EXPANSION='❯'
   # No line terminator if prompt_char is the last segment.
   typeset -g POWERLEVEL9K_PROMPT_CHAR_LEFT_PROMPT_LAST_SEGMENT_END_SYMBOL=
   # No line introducer if prompt_char is the first segment.
--- .zsh/105-misc.zsh
+++ .zsh/105-misc.zsh
@@ -3,11 +3,3 @@ export EDITOR=nvim
 if ! command -v nvim >/dev/null 2>&1; then
   export EDITOR=vim
 fi
-
-# Reduce delay when pressing Esc in vim mode
-if [[ -n $SSH_TTY || -n $SSH_CONNECTION || -n $MOSH_CONNECTION ]]; then
-  export KEYTIMEOUT=5    # 50 ms: protects Meta sequences over flaky SSH links
-else
-  export KEYTIMEOUT=1    # 10 ms: feels instant
-fi
-
