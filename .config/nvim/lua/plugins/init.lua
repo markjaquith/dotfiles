f9179bdd4288ed16599b437f1e5e9b2e093eecd3
Mark Jaquith <mark.jaquith@gusto.com>
2026-02-23 17:22:50 -0500

    Switch Copilot plugin

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -7,8 +7,7 @@ return {
   -- PLUGIN: Multi-cursor editing
   'mg979/vim-visual-multi',
 
-  -- PLUGIN: GitHub Copilot
-  'github/copilot.vim',
+  -- PLUGIN: GitHub Copilot (see plugins/copilot.lua)
 
   -- PLUGIN: I dunno, some sort of Lua thing?
   -- WARNING: Temporarily disabled to see if I can live without it.
