af7d3519337a1721fab34940c975b35a7bb6f24b
Mark Jaquith <mark@jaquith.me>
2026-06-29 16:04:32 -0400

    Replace delta with hunk as git pager

--- bin/dotfiles-install-configs.zsh
+++ bin/dotfiles-install-configs.zsh
@@ -5,10 +5,13 @@ mkdir -p "$HOME/Library/Application Support/lazygit"
 unlink "$HOME/Library/Application Support/lazygit/config.yml" 2>/dev/null
 ln -s "$HOME/.config/lazygit/config.yml" "$HOME/Library/Application Support/lazygit/config.yml" 2>/dev/null
 
-# Delta configuration
-if ! git config --global --get-regexp 'include\.path.*delta' >/dev/null 2>&1; then
-	git config --global --add include.path "~/.config/delta/themes/catppuccin-macchiato" >/dev/null 2>&1
-fi
+# Hunk as git pager (replaces delta)
+git config --global core.pager "hunk pager"
+git config --global --unset pager.diff 2>/dev/null
+git config --global --unset pager.show 2>/dev/null
+git config --global --unset pager.log 2>/dev/null
+# Remove legacy delta theme include if present
+git config --global --unset-all include.path "~/.config/delta/themes/catppuccin-macchiato" 2>/dev/null
 
 # Git aliases
 git config --global alias.fixup 'commit --all --amend --no-edit --no-verify'
