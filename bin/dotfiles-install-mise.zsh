b2248f2d9e723b3b8c6bf5b5ff7a059e98c95901
Mark Jaquith <mark@jaquith.me>
2026-07-03 16:31:03 -0400

    Use Herdr mise GitHub backend

--- bin/dotfiles-install-mise.zsh
+++ bin/dotfiles-install-mise.zsh
@@ -4,9 +4,13 @@ if ! command -v mise &> /dev/null
 then
 	echo "Installing mise..."
 	brew install mise
+elif brew list --formula mise &> /dev/null
+then
+	echo "Updating mise..."
+	brew upgrade mise
 fi
 
 # Install mise tools globally
-mise use -g herdr@latest > /dev/null
+mise use -g github:ogulcancelik/herdr > /dev/null
 mise use hk@latest > /dev/null
 mise use pkl@latest > /dev/null
