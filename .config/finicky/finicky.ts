2c18e618b994b8d88387d9f228e07bd21454368d
Mark Jaquith <mark@jaquith.me>
2026-08-03 18:47:31 -0400

    feat: route Google Meet to Google Meet PWA via router

--- .config/finicky/finicky.ts
+++ .config/finicky/finicky.ts
@@ -1,3 +1,12 @@
 export default {
 	defaultBrowser: "Google Chrome",
+	handlers: [
+		{
+			match: /^https?:\/\/meet\.google\.com(?:\/|$)/,
+			browser: {
+				name: "local.finicky.google-meet-router",
+				appType: "bundleId",
+			},
+		},
+	],
 }
