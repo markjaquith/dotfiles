e2f3a8dd041514018e5c59de9cb8b526509dfc7d
Mark Jaquith <mark@jaquith.me>
2026-08-04 12:20:27 -0400

    chore: remove redundant loading of files

--- .zsh/index.zsh
+++ .zsh/index.zsh
@@ -5,8 +5,8 @@ source $(dirname "$0")/000-init.zsh
 # ┃             Module         #┃ Description                  ┃ Condition      ┃
 # ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫
   ┃  005-profile-start         #┃ Start profiling              ┃ ZSH_PROFILE    ┃
-  ┃  010-paths                 #┃ Set paths                    ┃                ┃
-  ┃  015-tmux                  #┃ Tmux helpers                 ┃                ┃
+# ┃  010-paths                 #┃ Set paths                    ┃ early .zshrc   ┃
+# ┃  015-tmux                  #┃ Tmux helpers                 ┃ early .zshrc   ┃
   ┃  020-history               #┃ Configure zsh history        ┃                ┃
   ┃  040-pkgx                  #┃ Load Pkgx                    ┃                ┃
   ┃  050-bun                   #┃ Load Bun                     ┃                ┃
