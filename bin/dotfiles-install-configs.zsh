e09cc6f3a2e4395801d27825531f8cb96e060311
Mark Jaquith <mark@jaquith.me>
2026-09-01 13:31:49 -0400

    config: set Plannotator display name

--- bin/dotfiles-install-configs.zsh
+++ bin/dotfiles-install-configs.zsh
@@ -5,6 +5,17 @@ mkdir -p "$HOME/Library/Application Support/lazygit"
 unlink "$HOME/Library/Application Support/lazygit/config.yml" 2>/dev/null
 ln -s "$HOME/.config/lazygit/config.yml" "$HOME/Library/Application Support/lazygit/config.yml" 2>/dev/null
 
+# Plannotator identity
+if command -v jq &>/dev/null; then
+	mkdir -p "$HOME/.plannotator"
+	typeset plannotator_config="$HOME/.plannotator/config.json"
+	typeset plannotator_config_tmp
+	plannotator_config_tmp="$(mktemp "${plannotator_config}.XXXXXX")"
+	jq '.displayName = "Mark"' "$plannotator_config" 2>/dev/null >| "$plannotator_config_tmp" ||
+		jq -n '{ displayName: "Mark" }' >| "$plannotator_config_tmp"
+	mv "$plannotator_config_tmp" "$plannotator_config"
+fi
+
 # Hunk as git pager (replaces delta)
 git config --global core.pager "hunk pager"
 git config --global --unset pager.diff 2>/dev/null
@@ -25,5 +36,4 @@ fi
 # Git aliases
 git config --global alias.fixup 'commit --all --amend --no-edit --no-verify'
 git config --global alias.recent '!git reflog | grep "checkout: moving" | awk "!seen[\$NF]++ {print \$NF}"'
-
 
